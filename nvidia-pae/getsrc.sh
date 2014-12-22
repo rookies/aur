506ddbc4a44856e9673b14eba1070163bc84fbc2
Robert <robert@privatdemail.net>
2014-12-22 23:12:06 +0100

    removed nvidia-pae

--- nvidia-pae/getsrc.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-wget -c http://us.download.nvidia.com/XFree86/Linux-x86/$1/NVIDIA-Linux-x86-$1.run
-wget -c http://us.download.nvidia.com/XFree86/Linux-x86_64/$1/NVIDIA-Linux-x86_64-$1-no-compat32.run
-sha256sum NVIDIA-Linux-x86-$1.run NVIDIA-Linux-x86_64-$1-no-compat32.run
